03811fc37edbe4ef9df887dfd19b56eba87a6078
Robert Schoene <robert.schoene@tu-dresden.de>
2022-12-16 11:11:26 +0100

    added virt->phys

--- monitoring.c
+++ monitoring.c
@@ -50,6 +50,57 @@ static long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
 	return ret;
 }
 
+#define PAGEMAP_ENTRY 8
+#define GET_BIT(X,Y) (X & ((uint64_t)1<<Y)) >> Y
+#define GET_PFN(X) X & 0x7FFFFFFFFFFFFF
+
+/* from http://fivelinesofcode.blogspot.com/2014/03/how-to-translate-virtual-to-physical.html */
+unsigned long long read_pagemap(unsigned long long virt_addr){
+
+int i, c, pid, status;
+unsigned long virt_addr; 
+uint64_t read_val, file_offset;
+char path_buf [0x100] = {};
+FILE * f;
+char *end;
+  char path_buf[]="/proc/self/pagemap";
+   f = fopen(path_buf, "rb");
+   if(!f){
+      printf("Error! Cannot open %s\n", path_buf);
+      return -1;
+   }
+   
+   //Shifting by virt-addr-offset number of bytes
+   //and multiplying by the size of an address (the size of an entry in pagemap file)
+   file_offset = virt_addr / getpagesize() * PAGEMAP_ENTRY;
+   status = fseek(f, file_offset, SEEK_SET);
+   if(status){
+      perror("Failed to do fseek!");
+      return -1;
+   }
+   errno = 0;
+   read_val = 0;
+   unsigned char c_buf[PAGEMAP_ENTRY];
+   for(i=0; i < PAGEMAP_ENTRY; i++){
+      c = getc(f);
+      if(c==EOF){
+         printf("\nReached end of the file\n");
+         return 0;
+      }
+      if(is_bigendian())
+           c_buf[i] = c;
+      else
+           c_buf[PAGEMAP_ENTRY - i - 1] = c;
+      printf("[%d]0x%x ", i, c);
+   }
+   for(i=0; i < PAGEMAP_ENTRY; i++){
+      //printf("%d ",c_buf[i]);
+      read_val = (read_val << 8) + c_buf[i];
+   }
+   return read_val;
+   fclose(f);
+   return 0;
+}
 
 /* from https://stackoverflow.com/questions/39448276/how-to-use-clflush */
 /* from Reverse Engineering Intel Last-Level Cache Complex Addressing Using Performance Counters */
@@ -130,7 +181,7 @@ void monitor_cbo(){
     printf("\n");
   }*/
   for(char* address=array;address<&array[ARRAY_SIZE];address+=64){
-    printf(" %lli %d\n", address,values[(address-array)/64]);
+    printf(" %lli %d\n", read_pagemap(address),values[(address-array)/64]);
   }
 
   for(int i=0; i<nb_cores; i++){
